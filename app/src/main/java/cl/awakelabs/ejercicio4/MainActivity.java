359d84628331ca8d5b9f95fbc5442d36d7d90543
Alex Muñoz <lomejor77@gmail.com>
2023-06-24 23:05:48 -0400

    Ejercicio 4 Funcion suma agregado y probado

--- app/src/main/java/cl/awakelabs/ejercicio4/MainActivity.java
+++ app/src/main/java/cl/awakelabs/ejercicio4/MainActivity.java
@@ -3,6 +3,10 @@ package cl.awakelabs.ejercicio4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button suma = findViewById(R.id.btnSum);
+        Button resta = findViewById(R.id.btnRest);
+        Button multiplica = findViewById(R.id.btnMulti);
+        Button divide = findViewById(R.id.btnDiv);
+
+        EditText nUno = findViewById(R.id.numUno);
+        EditText nDos = findViewById(R.id.numDos);
+
+        suma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer uno = Integer.parseInt(nUno.getText().toString());
+                Integer dos = Integer.parseInt(nDos.getText().toString());
+
+                Integer suma = uno + dos;
+
+                Toast.makeText(getBaseContext(), "Resultado: "+ suma, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        resta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        multiplica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
